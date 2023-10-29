f5fb018d62e54e683be10607f2e6df562204c1cb
F0738353 Andre Luiz Paulino De Sousa <andrepaulinobtw@gmail.com>
2023-10-29 06:48:11 -0300

    feat: Aula 5

--- src/test/java/com/andrepaulino/service/SalaryRaiseServiceTest.java
+++ src/test/java/com/andrepaulino/service/SalaryRaiseServiceTest.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.andrepaulino.modelo.Funcionario;
@@ -12,30 +13,30 @@ import com.andrepaulino.modelo.Performance;
 
 public class SalaryRaiseServiceTest {
 
+    private SalaryRaiseService salaryRaise;
+    private Funcionario funci;
+
+    @BeforeEach
+    void setUp() {
+        this.salaryRaise = new SalaryRaiseService();
+        this.funci = new Funcionario("Vapo", LocalDate.now(), new BigDecimal("1000.00"));
+    }
+
     @Test
     public void raiseShouldBeThreePercentToLowPerformance() {
-        SalaryRaiseService salaryRaise = new SalaryRaiseService();
-        Funcionario funci = new Funcionario("Vapo", LocalDate.now(), new BigDecimal("1000.00"));
         salaryRaise.addRaise(funci, Performance.LOW);
-
         assertThat(funci.getSalario(), equalTo(new BigDecimal("1030.00")));
     }
 
     @Test
     public void raiseShouldBeFifteenPercentToGoodPerformance() {
-        SalaryRaiseService salaryRaise = new SalaryRaiseService();
-        Funcionario funci = new Funcionario("Vapo", LocalDate.now(), new BigDecimal("1000.00"));
         salaryRaise.addRaise(funci, Performance.GOOD);
-
         assertThat(funci.getSalario(), equalTo(new BigDecimal("1150.00")));
     }
 
     @Test
     public void raiseShouldBeTwentyPercentToGreatPerformance() {
-        SalaryRaiseService salaryRaise = new SalaryRaiseService();
-        Funcionario funci = new Funcionario("Vapo", LocalDate.now(), new BigDecimal("1000.00"));
         salaryRaise.addRaise(funci, Performance.GREAT);
-
         assertThat(funci.getSalario(), equalTo(new BigDecimal("1200.00")));
     }
 
